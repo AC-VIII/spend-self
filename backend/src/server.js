26ce715b134e7a770b7b86acc142055ae9be8a75
Araj Chalise <araj.chalise@gmail.com>
2026-08-08 15:10:21 +0545

    Updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,9 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
   })
 );
 
