90338c04f9e41d8193f5b0a9ebcaf8f86b90c7da
Araj Chalise <araj.chalise@gmail.com>
2026-08-09 12:18:29 +0545

    Updated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -88,7 +88,7 @@ async function submit(e: FormEvent) {
 
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/newsletter/subscribe`,
+      `${process.env.NEXT_PUBLIC_API_URL}/api/newsletter/subscribe`,
       {
         method: "POST",
         headers: {
